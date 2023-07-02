aec9e79772688221b54b567f4e04f0e40fe11ff9
Julia Neverova <julia.neverova@list.ru>
2023-07-02 23:09:24 +0700

    Adaptive for w425px

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,6 @@
-$('.menu_btn').on('click', function(e) {
-        e.preventDefault;
-        $(this).toggleClass('menu_btn_active');
+$(document).ready(function(){
+    $('.header_burger').click(function(event) {
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
-
-// $(document).ready(function(){
-//     $('.header_burger').click(function(event) {
-//         $('.header_burger, .header_menu').toggleClass('active');
-//         $('body').toggleClass('lock');
-//     });
-// });
